a460a9cbe52b76c7be5f82d82259ffdc78709153
Harshalkachewar <111994225+Harshalkachewar@users.noreply.github.com>
2025-12-01 18:45:26 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,8 @@
-# Add variables here if needed (not required now)
+variable "server_ip" {
+  default = "192.168.1.190"
+}
+
+variable "server_password" {
+  description = "Root password for Linux server"
+  sensitive   = true
+}
